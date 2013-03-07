b237c2d742d857f187b8bc24f38334e093d04003
stask <stask@me.com>
2013-03-07 09:14:50 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.stask/micro-middleware "0.0.4"
+(defproject org.clojars.stask/micro-middleware "0.0.5"
   :description "Collection of middlewares"
   :url "http://github.com/stask/micro-middleware"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "4.0.3"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [cheshire "5.0.2"]
                  [ring/ring-core "1.1.6"]]
   :profiles {:dev
              {:dependencies [[ring-mock "0.1.3"]]}})
